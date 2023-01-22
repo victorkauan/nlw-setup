bb730b7b2321392521ca26665b0f017105641fc1
Victor Kauan <victorkauan.dev@gmail.com>
2023-01-22 12:45:24 -0300

    feat: add minimum habit days size to SummaryTable
    - render habit days in SummaryTable up to fill 7 (seven) week, if necessary.

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -5,6 +5,9 @@ const weekDays = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
 
 const summaryDates = generateDatesFromYearBeginning();
 
+const minimumSummaryDatesSize = 18 * 7; // 18 weeks
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
+
 export function SummaryTable() {
   return (
     <div className="w-full flex">
@@ -25,6 +28,16 @@ export function SummaryTable() {
         {summaryDates.map((date) => {
           return <HabitDay key={date.toString()} />;
         })}
+
+        {amountOfDaysToFill > 0 &&
+          Array.from({ length: amountOfDaysToFill }).map((_, index) => {
+            return (
+              <div
+                key={index}
+                className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+              />
+            );
+          })}
       </div>
     </div>
   );
