a32bfa18ff9c52fbd161910e478dcbe4a12ad91c
Victor Kauan <victorkauan.dev@gmail.com>
2023-01-22 20:46:48 -0300

    feat: add transitions and focus styles

--- web/src/components/ProgressBar.tsx
+++ web/src/components/ProgressBar.tsx
@@ -11,7 +11,7 @@ export function ProgressBar(props: ProgressBarProps) {
         aria-valuemin={0}
         aria-valuemax={100}
         aria-valuenow={props.progress}
-        className="h-3 rounded-xl bg-violet-600"
+        className="h-3 rounded-xl bg-violet-600 transition-all"
         style={{ width: `${props.progress}%` }}
       ></div>
     </div>
