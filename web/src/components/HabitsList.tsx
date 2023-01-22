c93c374d58814ba53c94e174c286593417f788b6
Victor Kauan <victorkauan.dev@gmail.com>
2023-01-22 20:31:10 -0300

    feat: synchronize component styles in real time

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -7,6 +7,7 @@ import { api } from '../lib/axios';
 
 interface HabitsListProps {
   date: Date;
+  onCompletedChanged: (completed: number) => void;
 }
 
 interface HabitsInfo {
@@ -18,7 +19,7 @@ interface HabitsInfo {
   completedHabits: string[];
 }
 
-export function HabitsList({ date }: HabitsListProps) {
+export function HabitsList({ date, onCompletedChanged }: HabitsListProps) {
   const [habitsInfo, setHabitsInfo] = useState<HabitsInfo>();
 
   useEffect(() => {
@@ -53,6 +54,8 @@ export function HabitsList({ date }: HabitsListProps) {
       possibleHabits: habitsInfo!.possibleHabits,
       completedHabits,
     });
+
+    onCompletedChanged(completedHabits.length);
   }
 
   const isDateInPast = dayjs(date).endOf('day').isBefore(new Date());
