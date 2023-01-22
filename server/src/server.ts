7174c7d24fce1d9133b06b46bc541f53e12b563b
Victor Kauan <victorkauan.dev@gmail.com>
2023-01-22 11:18:38 -0300

    feat: create habit creation route
    - install Zod;
    - install Day.js;
    - separate the application in modules;
    - create habit creation HTTP route.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,31 +1,12 @@
 import cors from '@fastify/cors';
-import { PrismaClient } from '@prisma/client';
 import Fastify from 'fastify';
+import { appRoutes } from './routes';
 
 const app = Fastify();
 const port = 3333;
 
-const prisma = new PrismaClient();
-
 app.register(cors);
-
-app.get('/hello', () => {
-  return 'Hello, world!';
-});
-
-app.get('/habits', async () => {
-  // const drinkFilter = {
-  //   where: {
-  //     title: {
-  //       startsWith: 'Drink',
-  //     },
-  //   },
-  // };
-
-  const habits = await prisma.habit.findMany();
-
-  return habits;
-});
+app.register(appRoutes);
 
 app
   .listen({
